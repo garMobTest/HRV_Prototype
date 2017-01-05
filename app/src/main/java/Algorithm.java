bb8b73f230ad7b28256bf226319ac005bdb7df29
garrypolykoff <gpolyko@us.ibm.com>
2017-01-05 16:28:59 -0500

    adding Algorithm Class to calculate HRV score and ScoreTest to test it.

---
+++ app/src/main/java/Algorithm.java
@@ -0,0 +1,70 @@
+/**
+ * Created by garrypolykoff on 1/5/17.
+ */
+
+public class Algorithm {
+
+
+    /* these would be taken from some database based on a person age/sex, but for now
+     we just hardcode them
+     **/
+
+    private static final double LN_MEAN = 3.92;
+    private static final double LN_VAR = 0.64;
+    private static final double HRV_MEAN = 60.26;
+    private static final double HRV_VAR = 9.92;
+
+
+    public static int calculateHRV_Score(int[] hrvRaw_array) {
+
+
+        double score = 0.0;
+        double sum = 0.0;
+
+        for (int i = 0; i < hrvRaw_array.length -1; i++) {
+
+            sum += Math.pow((hrvRaw_array[i + 1] - hrvRaw_array[i]), 2);
+        }
+
+
+            // divide sum by number of measurements and take square root of it.
+         //   score = sqrt(sum / (hrvRaw_array.length -1));
+        // Log.e("Score:", score + "");
+
+// take natural logarithm of the number above – this is is the result.
+            score = Math.log10(Math.sqrt(sum / (hrvRaw_array.length -1)));
+        System.out.println("Inside Algorithm Class LN(rmssd): " + score );
+
+        return extrapolateScore(score, LN_MEAN,LN_VAR, HRV_MEAN, HRV_VAR);
+
+
+        }
+
+
+  private static int extrapolateScore (double rmssd, double ln_mean, double ln_variance, double score_mean, double score_variance) {
+
+
+      int result = 0;
+
+
+      double factor = (Math.abs(ln_mean - rmssd))/ln_variance;
+
+      if (rmssd < ln_mean) {
+
+
+          result = (int)(score_mean - score_variance * factor);
+      } else {
+
+
+          result = (int)(score_mean + score_variance * factor);
+      }
+
+      return result;
+
+  }
+
+
+
+
+
+}
---
+++ app/src/test/java/ScoreTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by garrypolykoff on 1/5/17.
+ */
+public class ScoreTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testScoreCalculation() {
+
+        int [] testArray = {474, 374, 122, 394, 374,363,897,253, 372};
+
+        int score = Algorithm.calculateHRV_Score(testArray);
+
+        System.out.println("Score: " + score);
+
+    }
+
+
+}
